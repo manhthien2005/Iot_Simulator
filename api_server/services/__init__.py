9e01a2b59b4ab3e109662da839d42edc88139b66
ThienPDM <phandienmanhthienk16@siu.edu.vn>
2026-04-05 14:28:10 +0700

    refactor(arch): extract SleepService from SimulatorRuntime [3.4]

--- api_server/services/__init__.py
+++ api_server/services/__init__.py
@@ -4,5 +4,6 @@ from api_server.services.device_service import DeviceService
 from api_server.services.vitals_service import VitalsService
 from api_server.services.alert_service import AlertService
 from api_server.services.session_service import SessionService
+from api_server.services.sleep_service import SleepService
 
-__all__ = ["DeviceService", "VitalsService", "AlertService", "SessionService"]
+__all__ = ["DeviceService", "VitalsService", "AlertService", "SessionService", "SleepService"]
